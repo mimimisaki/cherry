7fefd9596f15f36c7477032aa58aeed5ba5e51ca
mimimisaki <mimimisaki2@gmail.com>
2020-08-30 21:37:43 +0900

    [finish]chapter7.8

---
+++ chapter7/7_6_5.rb
@@ -0,0 +1,35 @@
+class Product
+    attr_reader :name, :price
+
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+
+    def to_s
+        "name: #{name}, price: #{price}"
+    end
+end
+product = Product.new('A great movie', 1000)
+puts product.to_s
+puts product.name
+puts product.price
+
+
+class DVD < Product
+    attr_reader :running_time
+
+    def initialize(name, price, running_time)
+        super(name, price)
+        @running_time = running_time
+    end
+    
+    def to_s
+        "#{super}, running_time: #{running_time}"
+    end
+end
+dvd = DVD.new('A great movie', 1000, 120)
+puts dvd.to_s
+puts dvd.name
+puts dvd.price
+puts dvd.running_time
